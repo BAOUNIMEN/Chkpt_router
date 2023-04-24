b2cc794065cd35336f0ec3e3bdada03cdd579d3e
BAOUNIMEN <benounimen91@gmail.com>
2023-04-24 23:51:14 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import MovieList from './composants/MovieList';
+import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Description from './composants/Description';
 
 function App() {
+
+  const [movie, setMovie] = useState([
+    {
+      id: Math.random(),
+      title: 'IRON MAN ',
+      description: 'disney',
+      posterURL: 'https://upload.wikimedia.org/wikipedia/en/0/02/Iron_Man_%282008_film%29_poster.jpg',
+      rating: 3
+    },
+
+    {
+      id: Math.random(),
+      title: 'IRON MAN 2',
+      description: 'série',
+      posterURL: 'https://fr.web.img2.acsta.net/medias/nmedia/18/70/45/28/19408942.jpg',
+      rating: 4
+    },
+
+    {
+      id: Math.random(),
+      title: 'IRON MAN 3',
+      description: 'série',
+      posterURL: 'https://fr.web.img5.acsta.net/medias/nmedia/18/91/08/37/20508296.jpg',
+      rating: 5
+    },
+
+    {
+      id: Math.random(),
+      title: 'IRON MAN 4',
+      description: 'série',
+      posterURL: 'https://i.ytimg.com/vi/QUfaHAR-Nho/maxresdefault.jpg',
+      rating: 5
+    },
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <MovieList test={movie} /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MovieList test={movie} />} />
+          <Route path="/movie/:id" element={<Description des={movie} />} />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
